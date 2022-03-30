0fe5e1aee0549d8dba1e048a0c766eabaf425674
jchen127 <jchen127@terpmail.umd.edu>
2022-03-30 15:26:49 -0400

    Setup Still 3

--- defectiveClient/src/app/headline/headline.component.ts
+++ defectiveClient/src/app/headline/headline.component.ts
@@ -1,43 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {WidthServiceService} from "../width-service.service";
+
 
 @Component({
   selector: 'app-headline',
   templateUrl: './headline.component.html',
   styleUrls: ['./headline.component.scss']
 })
-export class HeadlineComponent implements OnInit {
+export class HeadlineComponent implements OnInit, OnDestroy {
 
   detainJuvenileText: string;
+  juvenileDetained: number;
+
   detentionFacilityText: string;
+  detentionFacilityAvailable: number; //each detention facility can only take 50 juveniles
+
+  staffText: string;
+  staffsAvailable: number; //1 staff per 10 juveniles. Otherwise juveniles will be upset.
+
   logoText: string;
+
+
 
   constructor(private windowService: WidthServiceService) {
     this.detentionFacilityText = 'Detention Facility';
     this.detainJuvenileText = 'Detained Juvenile';
     this.logoText = 'Defective Juvenile Service';
+    this.staffText = "Available Staffs";
+    this.juvenileDetained = 0;
+    this.detentionFacilityAvailable = 0;
+    this.staffsAvailable = 0;
   }
 
   ngOnInit(): void {
 
-    console.log(`OnInit() ${window.innerWidth}`);
+
+
 
   }
 
-  /**
-   * TODO:
-   * this method needs to be implemented by service dependency injection so the calling method via *ngIf will structurally remove
-   * the content if ViewWidth is lesser than 768px
-   *
-   * @return {boolean} true or false
+  ngOnDestroy(): void {
+
+
+
+  }
+
+  /***
+   * This should be done via CSS because this is inherently not an interactive component.
+   * But for the sake of this project, I will try to make everything angular. Inherently
+   * non-interactive component that never change should be coded in CSS in my opinion.
    */
-  visibleIfGreaterThanXSWidth(): boolean{
+  GrayBorderFirstPill(): object{
 
-   // currently, set to true at all time, meaning visible at all times.
-
-    console.log(`Current width is notated as: ${this.windowService.getNativeWindow().screen.width}`);
-
-    return true;
+    return {'border-color': 'gray', 'border-style': 'solid', 'border-width': '1px'  };
   }
+
+  GrayBorderSecondPill(): object{
+
+    return {'border-style': 'solid solid solid none', 'border-color': 'gray', 'border-width': "1px"}
+
+  }
+
 
 }
